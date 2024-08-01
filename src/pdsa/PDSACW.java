9c77c0093ae41cf8fd4ba82d01661d6cf407b146
Rowell2002 <chethanarowell2002@gmail.com>
2024-08-01 20:01:52 +0530

    Created main class and added some example edges

--- src/pdsa/PDSACW.java
+++ src/pdsa/PDSACW.java
@@ -149,6 +149,22 @@ static class PathWithDistance {
     }
 }
 
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Graph graph = new Graph();
+
+        // Example edges
+        graph.addEdge("A", "B", 4);
+        graph.addEdge("A", "C", 2);
+        graph.addEdge("B", "C", 1);
+        graph.addEdge("B", "D", 5);
+        graph.addEdge("C", "D", 8);
+        graph.addEdge("C", "E", 10);
+        graph.addEdge("D", "E", 2);
+        graph.addEdge("D", "Z", 6);
+        graph.addEdge("E", "Z", 3);
+
 
 public class PDSACW {
     
